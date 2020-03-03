7fa6960f63dee8a3476813633893f6754647bc93
Demons Light <jetaimefrc@gmail.com>
2020-03-03 16:27:52 +0700

    feat: add cv detail information table

---
+++ sequelize/migrations/20200303091038-create-cv-detail.js
@@ -0,0 +1,57 @@
+'use strict'
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable(
+      'cvDetails',
+      {
+        id: {
+          type: Sequelize.UUID,
+          allowNull: false,
+          primaryKey: true,
+          defaultValue: Sequelize.literal('uuid_generate_v4()'),
+        },
+        cvId: {
+          allowNull: false,
+          type: Sequelize.UUID,
+          references: {
+            key: 'id',
+            model: 'curriculumVitaes',
+          },
+        },
+        /**
+         * Contains 2 value [X,Y] in cordinate system
+         */
+        coordinateAxis: {
+          allowNull: false,
+          type: Sequelize.ARRAY(Sequelize.TEXT),
+        },
+        detailInformation: {
+          allowNull: false,
+          type: Sequelize.JSON(),
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.NOW,
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.NOW,
+        },
+        deletedAt: {
+          allowNull: true,
+          type: Sequelize.DATE,
+        },
+      },
+      {
+        charset: 'utf-8',
+      },
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('cvDetails')
+  },
+}
