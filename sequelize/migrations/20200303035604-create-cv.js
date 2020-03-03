d620626a5da961177bb25603287b3a1445999ae7
Demons Light <jetaimefrc@gmail.com>
2020-03-03 16:26:41 +0700

    feat: update cv tables

--- sequelize/migrations/20200303035604-create-cv.js
+++ sequelize/migrations/20200303035604-create-cv.js
@@ -2,26 +2,28 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('curriculumVitaes', {
-      id: {
-        type: Sequelize.INTERGER,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
+    return queryInterface.createTable(
+      'curriculumVitaes',
+      {
+        id: {
+          type: Sequelize.UUID,
+          allowNull: false,
+          primaryKey: true,
+          defaultValue: Sequelize.literal('uuid_generate_v4()'),
+        },
+        avatarImage: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        userInfo: {
+          type: Sequelize.ARRAY(Sequelize.JSON()),
+          allowNull: false,
+        },
       },
-      avatarImage: {
-        type: Sequelize.STRING,
-        allowNull: false,
+      {
+        charset: 'utf-8',
       },
-      userInfo: {
-        type: Sequelize.ARRAY(Sequelize.JSON()),
-        allowNull: false,
-      },
-      cvDetail: {
-        type: Sequelize.ARRAY(Sequelize.JSON()),
-        allowNull: false,
-      },
-    })
+    )
   },
 
   down: (queryInterface, Sequelize) => {
