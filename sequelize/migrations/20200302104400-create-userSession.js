93f39ade4292581b1b2d7f0bd8dd010e00ad2eeb
Demons Light <jetaimefrc@gmail.com>
2020-03-03 16:30:38 +0700

    refactor(table): change format user session table

--- sequelize/migrations/20200302104400-create-userSession.js
+++ sequelize/migrations/20200302104400-create-userSession.js
@@ -1,37 +1,39 @@
 'use strict'
-
-module.exports.up = (queryInterface, Sequelize) => {
-  return queryInterface.createTable(
-    'userSessions',
-    {
-      id: {
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
-        type: Sequelize.INTEGER,
-      },
-      userId: {
-        allowNull: false,
-        references: {
-          key: 'id',
-          model: 'users',
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable(
+      'userSessions',
+      {
+        id: {
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+          type: Sequelize.INTEGER,
         },
-        type: Sequelize.INTEGER,
+        userId: {
+          allowNull: false,
+          references: {
+            key: 'id',
+            model: 'users',
+          },
+          type: Sequelize.INTEGER,
+        },
+        expiresAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.NOW,
+        },
       },
-      expiresAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
+      {
+        charset: 'utf8',
       },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.NOW,
-      },
-    },
-    {
-      charset: 'utf8',
-    },
-  )
+    )
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('userSessions')
+  },
 }
-
-module.exports.down = queryInterface => queryInterface.dropTable('userSessions')
