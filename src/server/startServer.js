e449dbb6dc65d3a2308a4dbb5799b5c59fe342b3
Demons Light <jetaimefrc@gmail.com>
2020-03-04 23:40:51 +0700

    feat: init express

--- src/server/startServer.js
+++ src/server/startServer.js
@@ -1,3 +1,13 @@
-import path from 'path'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import express from 'express'
+import passport from 'passport'
 
-console.log('Hello Babe')
+import accessEnv from '@helpers/accessEnv'
+const PORT = accessEnv('PORT', 6969)
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(passport.initialize())
+app.use(passport.session())
