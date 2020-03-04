71ba27a81de3190b8cb637a5095db32fe0334eb0
Demons Light <jetaimefrc@gmail.com>
2020-03-04 23:31:38 +0700

    feat: add passport strategies

---
+++ src/middleware/passport.js
@@ -0,0 +1,7 @@
+import passport from 'passport'
+import refresh from 'passport-oauth2-refresh'
+import { Strategy as LocalStrategy } from 'passport-local'
+import { Strategy as FacebookStrategy } from 'passport-facebook'
+import { OAuth2Strategy as GoogleStrategy } from 'passport-google-oauth'
+import { Strategy as LinkedInStrategy } from 'passport-linkedin-oauth2'
+import User from '@models/User'
