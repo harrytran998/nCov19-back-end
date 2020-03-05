ce7063935e3583e584c096b255bffabace51dcf1
Demons Light <jetaimefrc@gmail.com>
2020-03-05 11:47:47 +0700

    chore: test some routes

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,3 +1,5 @@
+import { User } from '../models'
+
 /**
  *
  * @param {import("express").Application} app
@@ -5,6 +7,14 @@
 const setupRoutes = app => {
   app.get('/', (req, res, next) => {
     return res.status(200).json({ message: 'Hello world' })
+  })
+  app.get('/users', async (req, res, next) => {
+    await User.findAll()
+      .then(users => {
+        return res.status(200).json(users)
+      })
+      .catch(err => console.log(err))
+    next()
   })
 }
 
