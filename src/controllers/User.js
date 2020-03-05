8a084382605a656db3ce813cfda229827a746cb6
Demons Light <jetaimefrc@gmail.com>
2020-03-05 17:05:32 +0700

    chore: update routes vs controller users

--- src/controllers/User.js
+++ src/controllers/User.js
@@ -1,12 +1,14 @@
 import { User } from '../models'
+
 /**
  *
  * @param {import('express').Request} req
  * @param {import('express').Response} res
  * @param {import('express').NextFunction} next
  */
-export const fetchAllUser = async (req, res, next) => {
-  await User.findAll()
+export const fetchUsers = async (req, res, next) => {
+  const { limit, offset } = req.pagination()
+  await User.findAll({ limit, offset })
     .then(users => {
       return res.status(200).json(users)
     })
--- src/server/routes.js
+++ src/server/routes.js
@@ -1,11 +1,11 @@
-import { fetchAllUser } from '@controllers/User'
+import { fetchUsers } from '@controllers/User'
 
 /**
  *
  * @param {import("express").Application} app
  */
 const setupRoutes = app => {
-  app.get('/users', fetchAllUser)
+  app.get('/users', fetchUsers)
 }
 
 export default setupRoutes
