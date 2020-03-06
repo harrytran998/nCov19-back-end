38ee388e57baa88b33191d406455b4daad299b47
Demons Light <jetaimefrc@gmail.com>
2020-03-06 19:54:01 +0700

    feat: init oauth2

---
+++ src/controllers/OAuth.js
@@ -0,0 +1,26 @@
+import { google } from 'googleapis'
+import fbgraph from 'fbgraph'
+
+/**
+ *
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next
+ */
+export const getFacebook = (req, res, next) => {}
+
+/**
+ *
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next
+ */
+export const getGoogle = (req, res, next) => {}
+
+/**
+ *
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next
+ */
+export const getLinkedIn = (req, res, next) => {}
