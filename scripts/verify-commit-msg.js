be5fbe679056e5acbc7d31541754e518cd994b5e
Demons Light <jetaimefrc@gmail.com>
2020-03-12 00:15:56 +0700

    style: update style for scripts

--- scripts/verify-commit-msg.js
+++ scripts/verify-commit-msg.js
@@ -1,7 +1,7 @@
-const chalk = require("chalk")
+const chalk = require('chalk')
 const msgPath = process.env.GIT_PARAMS
-const msg = require("fs")
-  .readFileSync(msgPath, "utf-8")
+const msg = require('fs')
+  .readFileSync(msgPath, 'utf-8')
   .trim()
 
 const commitMes = /^(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?: .{1,50}/
@@ -9,11 +9,11 @@ const commitMes = /^(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci
 if (!commitMes.test(msg)) {
   console.log()
   console.error(
-    `  ${chalk.bgRed.white(" ERROR ")} ${chalk.red(`invalid commit message format.`)}\n\n` +
-      chalk.red(`  Proper commit message format is required for automated changelog generation. Examples:\n\n`) +
-      `    ${chalk.green(`feat(nav): add algolia search option`)}\n` +
-      `    ${chalk.green(`fix(services-worker): handle pages URL pattern (close #28)`)}\n\n` +
-      chalk.red(`  See .github/commit-convention.md for more details.\n`)
+    `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red('invalid commit message format.')}\n\n` +
+      chalk.red('  Proper commit message format is required for automated changelog generation. Examples:\n\n') +
+      `    ${chalk.green('feat(nav): add algolia search option')}\n` +
+      `    ${chalk.green('fix(services-worker): handle pages URL pattern (close #28)')}\n\n` +
+      chalk.red('  See .github/commit-convention.md for more details.\n'),
   )
   process.exit(1)
 }
