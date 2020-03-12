08e02007080a43bc9ded8f7cd5341c042ac7f607
Demons Light <jetaimefrc@gmail.com>
2020-03-12 12:31:57 +0700

    feat: update cors middleware

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -1,10 +1,21 @@
 import cors from 'cors'
 
-const corsMiddleware = () => {
+export const corsMiddleware = () => {
   return cors({
     origin: (origin, cb) => cb(null, true),
     credentials: true,
   })
 }
 
-export default corsMiddleware
+export const setCorsHeader = (req, res, next) => {
+  // CORS headers
+  res.header('Access-Control-Allow-Origin', '*') // restrict it to the required domain
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE,OPTIONS')
+  // Set custom headers for CORS
+  res.header('Access-Control-Allow-Headers', 'Authorization,Content-type')
+  if (req.method === 'OPTIONS') {
+    res.status(200).send('OKIE !')
+  } else {
+    next()
+  }
+}
