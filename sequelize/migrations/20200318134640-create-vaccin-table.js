2027ad45f7f74d9cdc9afa0efdbd47e685ad52bf
Demons Light <jetaimefrc@gmail.com>
2020-03-18 22:05:56 +0700

    feat: add vaccins table migration

---
+++ sequelize/migrations/20200318134640-create-vaccin-table.js
@@ -0,0 +1,41 @@
+'use strict'
+
+module.exports = {
+  /**
+   * @param {import('sequelize/types').QueryInterface} queryInterface
+   */
+  up: (queryInterface, DataTypes) => {
+    return queryInterface.createTable(
+      'vaccins',
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        dateOfPublished: {
+          type: DataTypes.DATE,
+          allowNull: false,
+        },
+        dateOfManufacture: {
+          type: DataTypes.DATE,
+          allowNull: false,
+        },
+      },
+      {
+        charset: 'utf-8',
+      },
+    )
+  },
+  /**
+   * @param {import('sequelize/types').QueryInterface} queryInterface
+   */
+  down: (queryInterface, DataTypes) => {
+    return queryInterface.dropTable('vaccins')
+  },
+}
